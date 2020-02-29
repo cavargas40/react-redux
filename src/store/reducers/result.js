91ea4eba27b30cf973437e1e579a79c1de56bb04
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-28 19:51:33 -0500

    a leaner switchcase statement

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,18 +5,19 @@ const initialState = {
   results: []
 };
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    result => result.id !== action.payload
+  );
+  return updateObject(state, { results: updatedArray });
+};
+
 const resultReducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionTypes.StoreResult:
-      return updateObject(state, {
-        results: [...state.results, { id: new Date(), value: action.payload }]
-      });
+      return updateObject(state, { results: [...state.results, { id: new Date(), value: action.payload }] });
     case ActionTypes.DeleteResult:
-      const updatedResults = [...state.results].filter(
-        result => result.id !== action.payload
-      );
-
-      return updateObject(state, { results: updatedResults });
+      return deleteResult(state, action);
     default:
       return state;
   }
